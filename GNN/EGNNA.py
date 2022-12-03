da4c633e454ebb9c9c9a1ac0437f7f52b5c81d7a
zys <1239626906@qq.com>
2022-12-03 09:49:08 +0800

    bug fix

--- GNN/EGNNA.py
+++ GNN/EGNNA.py
@@ -6,17 +6,10 @@ def get_attention(x, adj):
     row_data = x.repeat(1,n)
     x_row = row_data.reshape(-1, m)
     x_col = x.repeat(n, 1)
-<<<<<<< HEAD
-    x_data = torch.cat((x_row, x_col), dim=1)
-    x_dataT = x_data.T
-    param_att = nn.Parameter(torch.FloatTensor(1, x_dataT.shape[1]))
-    att_data = torch.matmul(param_att, x_dataT)
-=======
     ## reshape to [n,n,2m]
     x_data = torch.cat((x_row, x_col), dim=1).view(n, -1, 2*m)
     param_att = nn.Parameter(torch.FloatTensor(x_dataT.shape[1],1))
     att_data = torch.matmul(x_dataT, param_att)
->>>>>>> EGNNA
     act = nn.LeakyReLU()
     att = torch.exp(act(att_data))
     x = torch.matmul(att, x)
