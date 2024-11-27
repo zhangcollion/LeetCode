9c5e5f69c1f01d9d9d43e6741e5c11b125a8d3b9
zhangcollion <1239626906@qq.com>
2024-11-27 21:42:26 +0800

    指针

--- C++_code/test.cpp
+++ C++_code/test.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
+// void PrintParaPoint(int a,  int& b){
+//     cout << "a = " << a<<",b= " << b << endl;
+//     cout << "addr b is " << &b <<endl;
+//     b = 3; // b为num的引用，修改b 同样修改num
+//     return;
+// }
+
+void PrintParaPoint(int a,  int* b){
+    cout << "a = " << a<<",b= " << *b << endl;
+    cout << "addr b is " << &b <<endl;
+    *b = 333; // b为num的引用，修改b 同样修改num
+    return;
+}
+
 int main()
 {
-    float a;
-    a = 3.0;
-    float b;
-    b = 4.0;
-    std::cout << "Hello World" << std::endl;
-    std::cout << "the first C++ code in vs code" << std::endl;
-    std::cout<< "ans out is " <<a+b<< std::endl;
+    int num = 1;
+    cout << "address of num: " << &num <<endl;
+    int newNum = num;
+    num = 2;
+    cout << "address of num: "  << &num << endl;
+    cout << "address of new num: "  << &newNum << endl;
+    cout << "num = " << num << "\nnewNum = " << newNum << endl; 
+    int& rNum  = num;  // int& 引用变量，rNum 为num的别名，对num 操作就是对rNum 操作
+    int* pNum = &num; // pNum 指针，指向num的内存地址； & 取值地址运算符
+    cout << "rNum: "  << rNum << endl;
+    cout << "pNum: " << *pNum << endl; // * 间接寻址运算符，将pNum 内存地址中的数据取出
+    *pNum = 1000;
+    cout << "pNum: " << *pNum << endl; // * 间接寻址运算符，将pNum 内存地址中的数据取出
+    cout << "num: " << num << endl; 
+    PrintParaPoint(num, &num);
+    cout << "num: "<<num << endl; 
+    return 0;
+
 }
